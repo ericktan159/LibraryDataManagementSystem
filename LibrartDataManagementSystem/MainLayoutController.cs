a6cd5b062f2434611486c3f47916ecd47fa0c55f
Johndell S. Kitts <k.jaydeee@gmail.com>
2022-07-08 15:38:41 +0800

    added new function overload for LoadForm to be reused of the booklayout

--- LibrartDataManagementSystem/MainLayoutController.cs
+++ LibrartDataManagementSystem/MainLayoutController.cs
@@ -9,7 +9,31 @@ namespace LibrartDataManagementSystem
 {
     class MainLayoutController
     {
+        /// <summary>
+        /// load form for main layout
+        /// </summary>
+        /// <param name="form">form of the layout we will show</param>
+        /// <param name="originalForm">the mainlayout form</param>
         public void LoadForm(Form form, MainLayout originalForm)
+        {
+            if (originalForm.GetMainPanel().Controls.Count > 0)
+            {
+                originalForm.GetMainPanel().Controls.RemoveAt(0);
+            }
+            Form f = form;
+            f.TopLevel = false;
+            f.Dock = DockStyle.Fill;
+            originalForm.GetMainPanel().Controls.Add(f);
+            originalForm.GetMainPanel().Tag = f;
+            f.Show();
+        }
+
+        /// <summary>
+        /// loadform for books
+        /// </summary>
+        /// <param name="form">content of what we want to use</param>
+        /// <param name="originalForm">the booklayoutform</param>
+        public void LoadForm(Form form, BooksLayoutForm originalForm)
         {
             if (originalForm.GetMainPanel().Controls.Count > 0)
             {
