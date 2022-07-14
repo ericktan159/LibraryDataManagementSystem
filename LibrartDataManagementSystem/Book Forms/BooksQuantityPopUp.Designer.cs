6c317ec796a050c35d95db062f08ecac5c7e83df
Johndell S. Kitts <k.jaydeee@gmail.com>
2022-07-14 20:35:49 +0800

    improve design for BooksQuantityPopUp

--- LibrartDataManagementSystem/Book Forms/BooksQuantityPopUp.Designer.cs
+++ LibrartDataManagementSystem/Book Forms/BooksQuantityPopUp.Designer.cs
@@ -33,6 +33,7 @@ namespace LibrartDataManagementSystem.Book_Forms
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.buttonSubmit = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
+            this.labelTitle = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,7 +41,7 @@ namespace LibrartDataManagementSystem.Book_Forms
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(112, 81);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(198, 18);
             this.label1.TabIndex = 0;
@@ -49,7 +50,7 @@ namespace LibrartDataManagementSystem.Book_Forms
             // numericUpDown1
             // 
             this.numericUpDown1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown1.Location = new System.Drawing.Point(16, 34);
+            this.numericUpDown1.Location = new System.Drawing.Point(115, 102);
             this.numericUpDown1.MaximumSize = new System.Drawing.Size(191, 0);
             this.numericUpDown1.Minimum = new decimal(new int[] {
             1,
@@ -71,7 +72,7 @@ namespace LibrartDataManagementSystem.Book_Forms
             this.buttonSubmit.BackColor = System.Drawing.Color.Aqua;
             this.buttonSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonSubmit.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonSubmit.Location = new System.Drawing.Point(16, 79);
+            this.buttonSubmit.Location = new System.Drawing.Point(115, 147);
             this.buttonSubmit.Name = "buttonSubmit";
             this.buttonSubmit.Size = new System.Drawing.Size(80, 25);
             this.buttonSubmit.TabIndex = 3;
@@ -83,25 +84,36 @@ namespace LibrartDataManagementSystem.Book_Forms
             this.buttonCancel.BackColor = System.Drawing.Color.Red;
             this.buttonCancel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonCancel.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonCancel.Location = new System.Drawing.Point(127, 79);
+            this.buttonCancel.Location = new System.Drawing.Point(226, 147);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(80, 25);
             this.buttonCancel.TabIndex = 4;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = false;
             // 
+            // labelTitle
+            // 
+            this.labelTitle.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(12, 9);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(378, 72);
+            this.labelTitle.TabIndex = 5;
+            this.labelTitle.Text = "id: 1; Book Title";
+            // 
             // BooksQuantityPopUp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(219, 116);
+            this.ClientSize = new System.Drawing.Size(402, 184);
+            this.Controls.Add(this.labelTitle);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonSubmit);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label1);
             this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(418, 223);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(235, 120);
+            this.MinimumSize = new System.Drawing.Size(418, 223);
             this.Name = "BooksQuantityPopUp";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Remove Quantity";
@@ -117,5 +129,6 @@ namespace LibrartDataManagementSystem.Book_Forms
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Button buttonSubmit;
         private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label labelTitle;
     }
 }
