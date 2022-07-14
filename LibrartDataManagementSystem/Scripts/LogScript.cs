149759f1541e2c62a18f2f1513d372d4a6d18d32
Johndell S. Kitts <k.jaydeee@gmail.com>
2022-07-14 14:32:10 +0800

    initialize variable to logscript

---
+++ LibrartDataManagementSystem/Scripts/LogScript.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrartDataManagementSystem.Scripts
+{
+    class LogScript
+    {
+        private LDMS_DataBaseController _databaseController = new LDMS_DataBaseController();
+
+        
+    }
+}
